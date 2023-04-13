f5f0c521c35a19441a76b5efc7d8d5bf2c7b5145
Vera <121445155+VeraSapozhnikova@users.noreply.github.com>
2023-04-13 15:39:26 +0300

    Delete yatube_api/scripts directory

--- yatube_api/scripts/isort.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from django.conf import settings
-from django_scripts.management.commands.cron import CronJobBaseCommand
-
-
-class Command(CronJobBaseCommand):
-    def cron_job(self):
-        os.system(f'{settings.BASE_DIR}/venv/bin/isort -rc .')
